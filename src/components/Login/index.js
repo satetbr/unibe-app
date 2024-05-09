fae81765e551771a7cceb926385fb50fc6a6efcb
satetbr <matheus.filipe.riobr@gmail.com>
2024-05-09 20:34:48 -0300

    Login screen

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -7,50 +7,52 @@ const logo = require("../../../assets/LOGO_BRANCA.png")
 
 export default function Login() {
     return (
-        <View>
             <ImageBackground
-            style={styles.frame}
+            style={styles.container}
             source={bg}>         
-                <KeyboardAvoidingView style={{backgroundColor: "#86895D"}}>
-                    <View>
+                <KeyboardAvoidingView
+                style={{ backgroundColor: "rgba(134,137,93,0.75)", width: 335, height: 420, borderRadius: 20, alignItems: "center"  }}>
+                    <View
+                    style={{marginTop:37}}>
                         <Image
-                        style={styles.imgLogo}
+                        style={{width: 205, height: 89}}
                         source={logo}/>
                     </View>
                     <View>
-                        <View>
-                            <Text>
+                        <View style={{marginTop:18, width: 260, height: 60}}>
+                            <Text style={{color: "rgba(35,25,25,1)", fontWeight: "800", fontSize: 15, marginLeft: 20}}>
                                 Matricula
                             </Text>
                             <TextInput
+                            keyboardType="numeric"
+                            style={{backgroundColor: "rgba(255,255,255,1)", borderRadius: 15, height: 43,}}
                             placeholder="Digite a sua matricula"
                             onChangeText={()=> {}}/>
                         </View>
-                        <View>
-                            <Text>
+                        <View style={{marginTop: 25}}>
+                            <Text style={{color: "rgba(35,25,25,1)", fontWeight: "800", fontSize: 15, marginLeft: 20}}>
                                 Senha
                             </Text>
                             <TextInput
+                            style={{backgroundColor: "rgba(255,255,255,1)", borderRadius: 15, height: 43,}}
                             placeholder="Digite a sua senha"
                             onChangeText={()=> {}}/>
-                            <Pressable>
-                                <Text>
+                            <Pressable style={{alignItems: "flex-end"}}>
+                                <Text style={{fontSize: 10, marginRight: 15, color: "rgba(255,255,255,0.8)", marginTop: 5}}>
                                     Esqueci a minha senha
                                 </Text>
                             </Pressable> 
                         </View>
-
                     </View>
-                    <View>
-                        <TouchableOpacity>
-                            <Text>Quero ser Aluno</Text>
+                    <View style={{flexDirection: "row", marginTop: 40, justifyContent: "space-around", paddingHorizontal: 19, width: 335}}>
+                        <TouchableOpacity style={{width: 120,  height: 36,  borderRadius: 70, backgroundColor: "rgba(52,82,54,1)", justifyContent: "center", alignItems: "center"}}>
+                            <Text style={{ color: "rgba(255,255,255,1)", FontSize: 14,  fontWeight: "800",}}>Quero ser Aluno</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity>
-                            <Text>Acessar</Text>
+                        <TouchableOpacity style={{width: 120,  height: 36,  borderRadius: 70, backgroundColor: "rgba(35,25,25,1)", justifyContent: "center", alignItems: "center"}}>
+                            <Text style={{color: "rgba(255,255,255,1)", fontSize: 14, fontWeight: "800"}}>Entrar</Text>
                         </TouchableOpacity>
                     </View>
                 </KeyboardAvoidingView>
             </ImageBackground>
-        </View>
     );
 }
--- src/components/Login/style.js
+++ src/components/Login/style.js
@@ -1,14 +1,11 @@
 import { StyleSheet } from "react-native";
 
 const style = StyleSheet.create ({
-    frame: {
-        width:"100%",
-        height:"100%",
-    },
-    imgLogo: {
-        width: 70,
-        height: 30,
-        margin: 50
+    container: {
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+        width: "100%"
     }
 });
 
