987a21c755bcf6ec2799d811b484f803fc4c2ae0
satetbr <matheus.filipe.riobr@gmail.com>
2024-05-11 21:15:27 -0300

    melhorias
    organizacao do codigo

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -11,45 +11,56 @@ export default function Login() {
             style={styles.container}
             source={bg}>         
                 <KeyboardAvoidingView
-                style={{ backgroundColor: "rgba(134,137,93,0.75)", width: 335, height: 420, borderRadius: 20, alignItems: "center"  }}>
+                style={styles.centralView}>
                     <View
-                    style={{marginTop:37}}>
+                    style={styles.viewLogo}>
                         <Image
-                        style={{width: 205, height: 89}}
+                        style={styles.logo}
                         source={logo}/>
                     </View>
                     <View>
-                        <View style={{marginTop:18, width: 260, height: 60}}>
-                            <Text style={{color: "rgba(35,25,25,1)", fontWeight: "800", fontSize: 15, marginLeft: 20}}>
+                        <View 
+                        style={styles.viewMatricula}>
+                            <Text 
+                            style={styles.textLabel}>
                                 Matricula
                             </Text>
                             <TextInput
                             keyboardType="numeric"
-                            style={{backgroundColor: "rgba(255,255,255,1)", borderRadius: 15, height: 43,}}
+                            style={styles.input}
                             placeholder="Digite a sua matricula"
                             onChangeText={()=> {}}/>
                         </View>
-                        <View style={{marginTop: 25}}>
-                            <Text style={{color: "rgba(35,25,25,1)", fontWeight: "800", fontSize: 15, marginLeft: 20}}>
+                        <View 
+                        style={styles.viewSenha}>
+                            <Text 
+                            style={styles.textLabel}>
                                 Senha
                             </Text>
                             <TextInput
-                            style={{backgroundColor: "rgba(255,255,255,1)", borderRadius: 15, height: 43,}}
+                            style={styles.input}
                             placeholder="Digite a sua senha"
                             onChangeText={()=> {}}/>
-                            <Pressable style={{alignItems: "flex-end"}}>
-                                <Text style={{fontSize: 10, marginRight: 15, color: "rgba(255,255,255,0.8)", marginTop: 5}}>
+                            <Pressable 
+                            style={styles.viewRecovery}>
+                                <Text 
+                                style={styles.textRecovery}>
                                     Esqueci a minha senha
                                 </Text>
                             </Pressable> 
                         </View>
                     </View>
-                    <View style={{flexDirection: "row", marginTop: 40, justifyContent: "space-around", paddingHorizontal: 19, width: 335}}>
-                        <TouchableOpacity style={{width: 120,  height: 36,  borderRadius: 70, backgroundColor: "rgba(52,82,54,1)", justifyContent: "center", alignItems: "center"}}>
-                            <Text style={{ color: "rgba(255,255,255,1)", fontSize: 14,  fontWeight: "800",}}>Quero ser Aluno</Text>
+                    <View 
+                    style={styles.viewButtons}>
+                        <TouchableOpacity 
+                        style={styles.buttonAluno}>
+                            <Text 
+                            style={styles.textButton}>Quero ser Aluno</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity style={{width: 120,  height: 36,  borderRadius: 70, backgroundColor: "rgba(35,25,25,1)", justifyContent: "center", alignItems: "center"}}>
-                            <Text style={{color: "rgba(255,255,255,1)", fontSize: 14, fontWeight: "800"}}>Entrar</Text>
+                        <TouchableOpacity 
+                        style={styles.buttonEntrar}>
+                            <Text 
+                            style={styles.textButton}>Entrar</Text>
                         </TouchableOpacity>
                     </View>
                 </KeyboardAvoidingView>
--- src/components/Login/style.js
+++ src/components/Login/style.js
@@ -6,6 +6,89 @@ const style = StyleSheet.create ({
         alignItems: "center",
         height: "100%",
         width: "100%"
+    },
+
+    centralView: {
+        backgroundColor: "rgba(134,137,93,0.75)", 
+        width: 335, 
+        height: 420, 
+        borderRadius: 20, 
+        alignItems: "center" 
+    },
+
+    viewLogo: {
+        marginTop:37
+    },
+
+    logo:{
+        width: 205, 
+        height: 89
+    },
+
+    viewMatricula: {
+        marginTop:18,
+        width: 260, 
+        height: 60
+    },
+
+    textLabel:{
+        color: "rgba(35,25,25,1)", 
+        fontWeight: "800",
+        fontSize: 15, 
+        marginLeft: 20
+    },
+
+    input: {
+        backgroundColor: "rgba(255,255,255,1)", 
+        borderRadius: 15, 
+        height: 43
+    },
+
+    viewSenha: {
+        marginTop: 25
+    },
+
+    viewRecovery: {
+        alignItems: "flex-end"
+    },
+
+    textRecovery: {
+        fontSize: 10, 
+        marginRight: 15, 
+        color: "rgba(255,255,255,0.8)", 
+        marginTop: 5
+    },
+
+    viewButtons: {
+        flexDirection: "row", 
+        marginTop: 40, 
+        justifyContent: "space-around",
+        paddingHorizontal: 19, 
+        width: 335
+    },
+
+    buttonAluno: {
+        width: 120,  
+        height: 36,  
+        borderRadius: 70, 
+        backgroundColor: "rgba(52,82,54,1)", 
+        justifyContent: "center", 
+        alignItems: "center"
+    },
+
+    textButton: {
+        color: "rgba(255,255,255,1)", 
+        fontSize: 14,  
+        fontWeight: "800"
+    },
+
+    buttonEntrar: {
+        width: 120,  
+        height: 36,  
+        borderRadius: 70, 
+        backgroundColor: "rgba(35,25,25,1)", 
+        justifyContent: "center", 
+        alignItems: "center"
     }
 });
 
